3dd2a7c564f0e06f3e4407e51f74341f104a8270
Karhal <mar.fucik@gmail.com>
2020-10-30 11:06:54 +0100

    Loading hero details on hero-detail component thru HeroService.getHeroe(id)

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,20 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+    ) { }
 
   ngOnInit(): void {
+
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHeroe(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
